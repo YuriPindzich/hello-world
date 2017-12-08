756d1eb2dcdb6d2ff8441b7ae45c971dd96687df
YuriPindzich <y_pindzich@iba.by>
2017-12-08 17:12:50 +0300

    my first build
    first commit and push

---
+++ src/aProject1/AclassMy.java
@@ -0,0 +1,8 @@
+package aProject1;
+public class AclassMy {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+     System.out.println("Hello World!");
+     System.out.println("How are you?");
+    }
+}
